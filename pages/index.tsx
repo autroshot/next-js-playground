198801028cb12c34adb3ce8096e686e8d4252536
autroshot <autroshot@gmail.com>
2023-10-27 04:38:16 +0900

    이미지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
+import Image from 'next/image';
 import { useState } from 'react';
+import TestImage from '/public/test-image.png';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +21,14 @@ export default function Home() {
           </button>
           <div>{count}</div>
         </div>
+        <Image src={TestImage} alt="테스트용 로컬 이미지" />
+        <Image
+          src="https://images.unsplash.com/photo-1501854140801-50d01698950b?auto=format&fit=crop&q=80&w=1575&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="테스트용 원격 이미지"
+          width={640}
+          height={381}
+          priority
+        />
       </div>
     </main>
   );
