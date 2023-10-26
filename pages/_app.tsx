bb1ccb4c90b3e1fd21e9a36ba2a9c19cbbf92c52
autroshot <autroshot@gmail.com>
2023-10-27 05:46:46 +0900

    글꼴 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Noto_Sans_KR } from 'next/font/google';
+import localFont from 'next/font/local';
+
+const notoSansKR = Noto_Sans_KR({
+  subsets: ['latin', 'latin-ext'],
+  variable: '--font-noto-sans-kr',
+});
+const pretendard = localFont({
+  src: '../public/pretendard.woff2',
+  variable: '--font-pretendard',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${notoSansKR.variable} ${pretendard.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,28 +9,40 @@ export default function Home() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className={`${inter.className}`}>
-      <div className="mx-auto max-w-screen-md py-10 space-y-3">
-        <h1 className="text-3xl font-bold">놀이터</h1>
-        <div className="flex items-center space-x-3">
-          <button
-            className="bg-amber-500 px-4 py-1 rounded-md"
-            onClick={handleClick}
-          >
-            버튼
-          </button>
-          <div>{count}</div>
-        </div>
-        <Image src={TestImage} alt="테스트용 로컬 이미지" />
-        <Image
-          src="https://images.unsplash.com/photo-1501854140801-50d01698950b?auto=format&fit=crop&q=80&w=1575&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="테스트용 원격 이미지"
-          width={640}
-          height={381}
-          priority
-        />
+    <div
+      className={`mx-auto max-w-screen-md py-10 space-y-3 ${inter.className}`}
+    >
+      <h1 className="text-3xl font-bold">놀이터</h1>
+      <div className="flex items-center space-x-3">
+        <button
+          className="bg-amber-500 px-4 py-1 rounded-md"
+          onClick={handleClick}
+        >
+          버튼
+        </button>
+        <div>{count}</div>
       </div>
-    </main>
+      <Image src={TestImage} alt="테스트용 로컬 이미지" />
+      <Image
+        src="https://images.unsplash.com/photo-1501854140801-50d01698950b?auto=format&fit=crop&q=80&w=1575&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="테스트용 원격 이미지"
+        width={640}
+        height={381}
+        priority
+      />
+      <p>
+        모든 사람은 의견의 자유와 표현의 자유에 대한 권리를 가진다. No one shall
+        be subjected to arbitrary arrest, detention or exile.
+      </p>
+      <p className="font-noto">
+        모든 사람은 의견의 자유와 표현의 자유에 대한 권리를 가진다. No one shall
+        be subjected to arbitrary arrest, detention or exile.
+      </p>
+      <p className="font-pretendard">
+        모든 사람은 의견의 자유와 표현의 자유에 대한 권리를 가진다. No one shall
+        be subjected to arbitrary arrest, detention or exile.
+      </p>
+    </div>
   );
 
   function handleClick() {
